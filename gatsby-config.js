885b844eb0573a3b5e6f0c73ce944196d88b0e83
LeoneBacciu <leonebacciu@gmail.com>
2021-07-16 14:27:23 +0200

    Add analytics and sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Leone Bacciu`,
+    description: `Leone Bacciu personal website`,
+    author: `@LeoneBacciu`,
+    siteUrl: `https://leonebacciu.github.io/`,
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ["G-N3X2BTSQ11"],
+      },
+    },
     `gatsby-plugin-no-sourcemaps`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
